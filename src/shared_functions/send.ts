8787b6de6f75d5144a20b3d2a4c97a4c33e5b41e
Wanderer2414 <huynguyenlequoc13@gmail.com>
2026-09-03 15:34:50 +0700

    fix; revert log

--- src/shared_functions/send.ts
+++ src/shared_functions/send.ts
@@ -1,8 +1,9 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
-import type { ApiError } from "../configs/errors.js";
+import { errors, type ApiError } from "../configs/errors.js";
 import type { ApiResponse } from "../definitions/responses.js";
 import { getBody, getHeader } from "./request.js";
 import { getTime } from "./get_time.js";
+import database_pool from "../controllers/db_router.js";
 
 // FN_REQUEST_LOG_INSERT(
 //     p_endpoint       VARCHAR(60),
@@ -21,17 +22,17 @@ export function sendJson(
   res.writeHead(status, { "Content-Type": "application/json" });
   res.end(JSON.stringify(body));
 
-  if (process.env.SNAP2CARD_SKIP_REQUEST_LOG === "1") return;
-  
-  void import("../controllers/db_router.js")
-    .then(async ({ default: database_pool }) => {
-      const requestBody = isMultipart(req) ? "[multipart body omitted]" : await getBody(req);
-      return database_pool.query("SELECT FN_REQUEST_LOG_INSERT($1, $2, $3, $4, $5)", [req.url!, getHeader(req), requestBody, status.toString(), JSON.stringify(body)]);
+  try {
+    database_pool.query("SELECT FN_REQUEST_LOG_INSERT($1, $2, $3, $4, $5)", [req.url!, getHeader(req), getBody(req), status.toString(), JSON.stringify(body)]).catch((e) => {
+      console.log("DB Error: ", e)
+      throw e
     })
-    .catch((e) => {
-      console.log("DB Error: ", e);
-    });
-  console.log(getTime(), ": New request comming, logged into database: ", req.url);
+  }
+  catch (e) {
+    console.log("Error: ", e)
+    sendError(req, res, errors.invalidInputData)
+  }
+  console.log(getTime(), ": New request comming, logged into database: ", req.url)
 }
 
 function isMultipart(req: IncomingMessage): boolean {
